9796790a712d247c8f859c9fcb50d0ded15a6923
Linh Hoang <ellehoang@hotmail.com>
2021-11-20 16:16:13 -0500

    modify tweet object to be array of tweet objects and implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,61 +7,99 @@
 // take obj directly from initial-tweets.json
 // use jQuery to construct new elements using $ function
 
-// Test /driver code (temp). Eventually will get from server
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Mark",
+      "avatars": "https://i.ibb.co/gDfksWz/users-1M.png",
+      "handle": "@JSknight" },
+    "content": {
+      "text": "Christmas is just around the corner!"
+    },
+    "created_at": 1628618888939
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Carmin",
+      "avatars": "https://i.ibb.co/j6rMTyt/users-12-F.png",
+      "handle": "CSSguru" },
+    "content": {
+      "text": "OMG!!! My project is overdue :("
+    },
+    "created_at": 1628618888939
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.ibb.co/4M2fCM8/users-7M.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.ibb.co/7RS0tCM/users-14-F.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 $(document).ready(function() {
-
+  
   const createTweetElement = function(tweetObj) {
     //const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-    // create HTMl markup using template literals or template strings
     
+    // create HTMl markup using template literals or template strings
     const { name, avatars, handle } = tweetObj.user;
     const { text } = tweetObj.content;
     const time  = tweetObj.created_at;
     const convertTime = timeago.format(time);
     console.log(convertTime);
-
-    const $tweet =$(`
-      <article class="tweet">
-        <header>
-          <span class="tweeter"><img class="users-avatar" src=${avatars}>${name}</span>
-          <span class="handle">${handle}</span>
-        </header>
-        <p class="tweet-body">${text}</p>
-        <footer class="tweet-info">
-          <span>${convertTime}</span>
-          <div class="icons">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-          </div>
-        </footer>
-      </article>
+    
+    let $tweet =$(`
+    <article class="tweet">
+    <header>
+    <span class="tweeter"><img class="users-avatar" src=${avatars}>${name}</span>
+    <span class="handle">${handle}</span>
+    </header>
+    <p class="tweet-body">${text}</p>
+    <footer class="tweet-info">
+    <span>${convertTime}</span>
+    <div class="icons">
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
+    </div>
+    </footer>
+    </article>
     `);
     return $tweet;
   };
+  
+  const renderTweets = function(tweets) {
+    // loop through tweets
+    // call createTweetElement for each tweet
+    // take return value & appends it to tweet container (.all-tweets)
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('.all-tweets').append($tweet); // to add to pg to make sure it's got all right elements, classes, etc.
+    }
+  };
 
-  const $tweet = createTweetElement(tweetData);
+  renderTweets(data);
   
   // Test / driver code (temp)
-  console.log($tweet); // to see what it looks like
-  
-  $('.all-tweets').append($tweet); // to add to pg to make sure it's got all right elements, classes, etc.
-});
+  //console.log($tweet); // to see what it looks like
 
-/*
-return tweet<article> element; // contain entire HTML structure of tweet
-};
-*/
+  //return tweet<article> element;
+  // contain entire HTML structure of tweet
+});
