d3af134f31b4f4c0e6fae4019711b58add668134
Linh Hoang <ellehoang@hotmail.com>
2021-12-07 17:05:09 -0500

    add comment explaining codes and indenting html tweet block

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,8 @@
  */
 
 $(document).ready(function() {
+  
+  // create HTML tweet element that display time since made
   const createTweetElement = function(tweetObj) {
     const { name, avatars, handle } = tweetObj.user;
     const { text } = tweetObj.content;
@@ -12,32 +14,37 @@ $(document).ready(function() {
     const convertTime = timeago.format(time);
     console.log(convertTime);
     
+    // prevent XSS attack
     const escape = function(str) {
       let div = document.createElement("div");
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     };
     
+    // html tweet block
     let $tweet = $(`
-    <article class="tweet">
-    <header>
-    <span class="tweeter"><img class="users-avatar" src=${escape(avatars)}>${escape(name)}</span>
-    <span class="handle">${escape(handle)}</span>
-    </header>
-    <p class="tweet-body">${escape(text)}</p>
-    <footer class="tweet-info">
-    <span>${escape(convertTime)}</span>
-    <div class="icons">
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-    </div>
-    </footer>
-    </article>
+      <article class="tweet">
+        <header>
+          <span class="tweeter"><img class="users-avatar" src=${escape(avatars)}>${escape(name)}</span>
+          <span class="handle">${escape(handle)}</span>
+        </header>
+        
+        <p class="tweet-body">${escape(text)}</p>
+      
+        <footer class="tweet-info">
+          <span>${escape(convertTime)}</span>
+          <div class="icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
     `);
     return $tweet;
   };
   
+  // loop through tweet data to render dynamically
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
@@ -45,6 +52,7 @@ $(document).ready(function() {
     }
   };
   
+  // GET request to retrieve tweets
   const loadTweets = function() {
     $.ajax("/tweets", { type: "GET"})
       .then(function(data) {
@@ -52,16 +60,23 @@ $(document).ready(function() {
       });
   };
   loadTweets();
-
+  
+  // hide tweet form error message 
   $("#err-msg").hide();
+
+  // tweet form submission handler
   $("form").submit(function(event) {
     event.preventDefault();
+
+    // slide down and fade out error message
     if ($("#tweet-text").val() === "" || $("#tweet-text").val() === null) {
       return $("#err-msg").slideDown().fadeOut(5000).find("p").text(`We can not see what you're humming about.`);
     }
     if ($("#tweet-text").val().length > 140) {
       return $("#err-msg").slideDown().fadeOut(5000).find("p").text(`Your humming is way too long.`);
     }
+
+    // convert form data into query strng serialized data
     const queryStr = $(this).serialize();
     console.log(queryStr);
     $.ajax("/tweets", { type: "POST", data: queryStr})
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,20 @@
+// get jQuery DOM ready
 $(document).ready(function() {
   
-  console.log("character counter file is loaded!!!!!");
+  //console.log("character counter file is loaded!!!!!");
 
+  // tweet input chracters counter
   $("#tweet-text").on("input", function(e) {
     const numOfChar = (140 - $(this).val().length);
     
+    // handle css styling when chracter exceeds number of character
     if (numOfChar < 0) {
       $(".counter").addClass("exceed-count").html(numOfChar);
     } else {
       $(".counter").removeClass("exceed-count").html(numOfChar);
     }
+    
+    // dynamically change counter on html
     $(".counter").html(numOfChar);
   });
   
